367c1545c348dd5b8af6a8da6e0bd25f7a5d1193
김정은 <bluesara44@gmail.com>
2022-02-24 10:50:07 +0900

    [Update.] router, breadcrumb 수정 (2)

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import fota from './fota';
-import { useRoutes } from 'react-router-dom';
 import iotProtocol from './iotProtocol';
 import adminManage from './adminManage';
 import auth from './auth';
 import dashboard from './dashboard';
+import { useRoutes } from 'react-router-dom';
 
 export const nav = [dashboard, fota, iotProtocol, adminManage, auth];
 
