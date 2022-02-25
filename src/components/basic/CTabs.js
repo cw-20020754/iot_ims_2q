d46734c0c0ded3ac4bf5f2ad06151d67e290c8f4
hipark <hipark@coway.com>
2022-02-25 13:35:10 +0900

    - mui/lab lib 추가 - 커스텀 tab 추가 - tab 활용 샘플 추가

---
+++ src/components/basic/CTabs.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Tab, Box, Card } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
+
+const CTabs = (props) => {
+  const { tabDataList, sx, orientation, children } = props;
+
+  const [value, setValue] = React.useState(tabDataList[0].value);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Card>
+      <TabContext value={value}>
+        <Box
+          sx={
+            orientation === 'vertical'
+              ? {
+                  flexGrow: 1,
+                  display: 'flex',
+                }
+              : { borderBottom: 1, borderColor: 'divider' }
+          }
+        >
+          <TabList
+            onChange={handleChange}
+            orientation={orientation}
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+          >
+            {tabDataList.map((tab, index) => (
+              <Tab key={index} value={tab.value} label={tab.label} />
+            ))}
+          </TabList>
+
+          {tabDataList.map((tab, index) => (
+            <TabPanel key={index} value={tab.value}>
+              {children[index]}
+            </TabPanel>
+          ))}
+        </Box>
+      </TabContext>
+    </Card>
+  );
+};
+
+export default CTabs;
--- src/pages/AdminManage/LayoutSample.js
+++ src/pages/AdminManage/LayoutSample.js
@@ -7,6 +7,7 @@ import {
   CardActions,
 } from '@mui/material';
 import CButton from '../../components/basic/CButton';
+import CTabs from '../../components/basic/\bCTabs';
 import SampleDialog from './CustomDialogs/SampleDialog';
 
 const LayoutSample = () => {
@@ -19,6 +20,20 @@ const LayoutSample = () => {
   const dialogClose = () => {
     setOpen(false);
   };
+
+  const tabDataList = [
+    { value: '0001', label: '연결' },
+    { value: '0002', label: '일반기능 구현' },
+    { value: '0003', label: '센서기능 구현' },
+    { value: '0004', label: '에러' },
+    { value: '0005', label: '파라미터 변경' },
+    { value: '0006', label: 'FOTA' },
+    { value: '0007', label: '인증만료' },
+    { value: '0008', label: '스마트진단(상시)' },
+    { value: '0009', label: '스마트진단(정밀)' },
+    { value: '0010', label: '제품 데이터 동기화' },
+    { value: '0011', label: '부가정보요청' },
+  ];
 
   return (
     <Grid
@@ -71,6 +86,13 @@ const LayoutSample = () => {
         </Grid>
       </Grid>
 
+      <Grid item xs={12}>
+        <CTabs tabDataList={tabDataList}>
+          {/* tabDataList index에 맞게 component 추가 (children으로 render) */}
+          <Typography variant="h4">panel1</Typography>
+          <Typography variant="h4">panel2</Typography>
+        </CTabs>
+      </Grid>
       <SampleDialog open={open} onClose={dialogClose}></SampleDialog>
     </Grid>
   );
